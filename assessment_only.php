73502acb716c4e31edde8edb1aa76e116964d11a
Steven Smith <S.Smith2@pcs.hants.sch.uk>
2019-02-21 10:32:13 +0000

    Assessment Only Page Added

---
+++ assessment_only.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <!-- HEAD -->
+    <?php include("includes/head.html") ?>
+    <title>The Solent SCITT | Assessment Only</title>
+  </head>
+  <body>
+    <!-- TOPNAV -->
+    <?php include("includes/topnav.html") ?>
+
+    <!-- HEADER -->
+    <div id="page-header">
+      <header class="row">
+        <div class="columns">
+          <h1>Assessment Only</h1>
+        </div>
+      </header>
+    </div>
+    <!-- Section1 -->
+    <section class="row">
+      <div class="columns">
+        <h2>Assessment Only Route</h2>
+        <p>The Assessment Only Route has been developed for very experienced classroom practitioners who can demonstrate that they meet all the standards for QTS without teacher training.</p>
+
+        <h3>Are you a graduate working in school as an unqualified teacher?</h3>
+        <p>Can you answer 'yes' to the following statements?</p>
+        <ol>
+          <li>I have at least two years’ experience teaching in the classroom</li>
+          <li>I have taught in at least two different schools</li>
+          <li>I have GCSE C/4 or above in English and Maths (also GCSE C/4 in Science if you are teaching primary)</li>
+        </ol>
+      </div>
+    </section>
+    <!-- Section2 -->
+    <div class="accent-bg">
+      <section class="row">
+        <div class="columns">
+          <h3>The Assessment Only route could be for you.</h3>
+          <p>What you need to do:</p>
+          <ol>
+            <li>Speak to your current school and make sure that they are happy to support you. It will cost £2,500, payable in advance, and you will need a letter of support from your headteacher.</li>
+            <li>Apply to complete the professional skills test in literacy and numeracy</li>
+            <li>Put together a portfolio of evidence demonstrating that you have met the Teachers’ Standards.</li>
+            <li>Contact the Solent SCITT for an application form.</li>
+          </ol>
+
+          <h3>What happens next?</h3>
+          <p>Once we have received your application form, one of our SCITT assessors will come to your school to meet you and will need to see evidence of your qualifications, school experience and passing the skills tests.</p>
+          <p>We will then conduct a pre-registration observation of you teaching one of your classes.</p>
+
+          <h3>Why do we do this?</h3>
+          <p>We need to be satisfied that you will be able to meet the rigorous Teachers’ Standards without any additional training. </p>
+          <p>If this is not the case, we would recommend that you complete a one-year Inital Teacher Training (ITT) course via The Solent SCITT.</p>
+        </div>
+      </section>
+      <br>
+    </div>
+    <!-- Section3 -->
+    <section class="row">
+      <div class="columns">
+        <h3>Maximum timeframe from start to finish 12 weeks</h3>
+        <p>Once we are happy that you will meet the standards you will have two further observations in week four and eight with the SCITT assessor and the subject assessor from your home school.</p>
+        <p>Your final observation will be with the SCITT moderator.</p>
+        <p>Provided you have met all the standards you will be awarded Qualified Teacher Status (QTS).</p>
+        <p>To request an Assessment Only application form, please contact Phil Seery via Email: <a href="javascript:location='mailto:\u0069\u006e\u0066\u006f\u0040\u0074\u0068\u0065\u0073\u006f\u006c\u0065\u006e\u0074\u0073\u0063\u0069\u0074\u0074\u002e\u0063\u006f\u002e\u0075\u006b';void 0"><script type="text/javascript">document.write('\u0069\u006e\u0066\u006f\u0040\u0074\u0068\u0065\u0073\u006f\u006c\u0065\u006e\u0074\u0073\u0063\u0069\u0074\u0074\u002e\u0063\u006f\u002e\u0075\u006b')</script></a> or <a href="tel://02392-489819">02392 489 819</a>.</p> 
+      </div>
+    </section>
+
+    <!-- FOOTER -->
+    <?php include("includes/footer.html") ?>
+
+
+    <!-- DO NOT REMOVE -->
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
