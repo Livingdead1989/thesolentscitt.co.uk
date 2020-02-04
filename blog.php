112a57d9c3c4116c1970524503573f39e4a80ff3
S Smith <S.Smith2@pcs.hants.sch.uk>
2020-02-04 13:49:24 +0000

    blog page and first blog post

---
+++ blog.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <!-- HEAD -->
+    <?php include("includes/head.html") ?>
+    <title>The Solent SCITT | Blog</title>
+  </head>
+  <body>
+    <!-- TOPNAV -->
+    <?php include("includes/topnav.html") ?>
+
+    <!-- HEADER -->
+    <div id="page-header">
+      <header class="row">
+        <div class="columns">
+          <h1 class="capitalize">Blog</h1>
+        </div>
+      </header>
+    </div>
+
+    <!-- INSERT BLOG -->
+    <?php include("includes/blog/2020-02-04-the-importance-of-mfl.html") ?>
+
+    <section class="">
+      <div class="row">
+        <div class="columns">
+          <br>
+          <?php include 'includes/backtotop.html' ?>
+        </div>
+      </div>
+    </section>
+    <!-- FOOTER -->
+    <?php include("includes/footer.html") ?>
+
+
+    <!-- DO NOT REMOVE -->
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
