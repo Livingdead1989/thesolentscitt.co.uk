bf41104ebc7e2c64c96f8e17355e0dc262b0c326
LivingDead1989 <steven.smith1989@hotmail.co.uk>
2021-01-25 12:10:12 +0000

    +draft faq page

---
+++ faq.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <!-- HEAD -->
+    <?php include("includes/head.html") ?>
+    <title>The Solent SCITT | FAQ</title>
+  </head>
+  <body>
+    <!-- TOPNAV -->
+    <?php include("includes/topnav.html") ?>
+
+    <!-- HEADER -->
+    <div id="page-header">
+      <header class="row">
+        <div class="columns">
+          <h1 class="capitalize">FAQ</h1>
+        </div>
+      </header>
+    </div>
+
+    <section class="row">
+      <div class="columns">
+        <h2>Frequently Asked Questions</h2>
+
+        <ul class="vertical menu accordion-menu" data-accordion-menu>
+
+          <!-- <li>
+            <a href="#">Question</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li> -->
+
+          <li>
+            <a href="#">Why should I choose The Solent SCITT?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">What is a SCITT programme? What is the difference with a University-led route?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">What are the qualification entry requirements?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">Do you offer primary and secondary courses?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">For secondary courses do I need a degree in the subject I am applying for?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">Can I gain the PGCE award along with QTS?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">Is there funding available?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">Do I need school experience?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">Where will I be placed for my training?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">How long does training last?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">When should I apply?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">What are you looking for in a trainee teacher?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">Who do you train?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">Do you offer the Assessment Only route?</a>
+            <ul class="menu vertical nested">
+              <p>Answer</p>
+            </ul>
+          </li>
+
+        </ul>
+      </div>
+    </section>
+
+    <!-- Back to Top -->
+    <?php include 'includes/backtotop.html'; ?>
+
+    <!-- FOOTER -->
+    <?php include("includes/footer.html") ?>
+
+
+    <!-- DO NOT REMOVE -->
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
