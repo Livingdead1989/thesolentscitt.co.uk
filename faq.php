3ae7264254ca87d14edce4117ec7b210ad482d59
LivingDead1989 <steven.smith1989@hotmail.co.uk>
2021-01-26 14:34:43 +0000

    +FAQ Answers added

--- faq.php
+++ faq.php
@@ -34,98 +34,102 @@
           <li>
             <a href="#">Why should I choose The Solent SCITT?</a>
             <ul class="menu vertical nested">
-              <p>Answer</p>
-            </ul>
-          </li>
-
-          <li>
-            <a href="#">What is a SCITT programme? What is the difference with a University-led route?</a>
-            <ul class="menu vertical nested">
-              <p>Answer</p>
-            </ul>
-          </li>
-
-          <li>
-            <a href="#">What are the qualification entry requirements?</a>
-            <ul class="menu vertical nested">
-              <p>Answer</p>
-            </ul>
-          </li>
-
-          <li>
-            <a href="#">Do you offer primary and secondary courses?</a>
-            <ul class="menu vertical nested">
-              <p>Answer</p>
-            </ul>
-          </li>
-
-          <li>
-            <a href="#">For secondary courses do I need a degree in the subject I am applying for?</a>
-            <ul class="menu vertical nested">
-              <p>Answer</p>
+              <p>We are an experienced provider focused on high-quality ‘hands on’ training with the opportunity to learn from real teachers in real classrooms. We have strong school networks and our committed to helping the cohort find suitable teaching posts with many SCITT graduates employed in partnership and placement schools.</p>
             </ul>
           </li>
 
           <li>
             <a href="#">Can I gain the PGCE award along with QTS?</a>
             <ul class="menu vertical nested">
-              <p>Answer</p>
+              <p>Yes, all trainees who complete the course with be awarded Qualified Teacher Status (QTS) and there is an option to achieve as Post Graduate Certificate in Education (PGCE) accredited by the University of Portsmouth.</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">What is a SCITT programme? What is the difference with a University-led route?</a>
+            <ul class="menu vertical nested">
+              <p>A SCITT or school-centred route means you are in school from the September start and the emphasis is on mixed important academic theory with on the job learning alongside high-quality teachers. University-led course involves more classroom-based teaching, theory and reduced school days.</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">What are the qualification entry requirements?</a>
+            <ul class="menu vertical nested">
+              <p>To apply for a teacher training course, you must have GCSE English and Maths at Level 4 (Grade C) or above for all courses. If you wish to train for primary, you will also need a Level (Grade C) or above in a GCSE Science subject. We accept equivalency tests and can help arrange these for you if required.</p>
+
+              <p>You must hold a UK degree or equivalent to access this post graduate course and qualifications achieved abroad will need to be verified via NARIC. It is advantageous if the degree relates to the subject you wish to teach but we understand that sometimes this is not possible. All degree subjects and classifications are welcome.</p>
+
+              <p>All qualifications should be in place before the course starts in September.</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">Do you offer primary and secondary courses?</a>
+            <ul class="menu vertical nested">
+              <p>Yes, we offer courses for both primary and secondary. SCITT primary places are available each year and there are school direct salaried places. For secondary we offer both SCITT and School Direct courses. For all course information look at our <a href="courses.php">Courses page</a>.</p>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">For secondary courses do I need a degree in the subject I am applying for?</a>
+            <ul class="menu vertical nested">
+              <p>It is preferable to have a degree directly related to the subject you would like to teach but we understand this is not always possible. Subject knowledge enhancement courses are available in some areas and an assessment of subject knowledge will be made during the interview.</p>
             </ul>
           </li>
 
           <li>
             <a href="#">Is there funding available?</a>
             <ul class="menu vertical nested">
-              <p>Answer</p>
+              <p>Tax-free bursaries are available for some secondary subjects and tuition fee and maintenance loans are available through student finance to cover your training and living costs.</p>
             </ul>
           </li>
 
           <li>
             <a href="#">Do I need school experience?</a>
             <ul class="menu vertical nested">
-              <p>Answer</p>
+              <p>It is not a requirement to have school experience but history has shown us that it is beneficial for teacher training and we can help organise school experience in our partnership schools.</p>
             </ul>
           </li>
 
           <li>
             <a href="#">Where will I be placed for my training?</a>
             <ul class="menu vertical nested">
-              <p>Answer</p>
+              <p>You will have two placements and we endeavour to do a best fit approach for placements. We consider travel distance for candidates, school capacity and both trainee and school needs. If you prefer a specific school placement this can be discussed during interview.</p>
             </ul>
           </li>
 
           <li>
             <a href="#">How long does training last?</a>
             <ul class="menu vertical nested">
-              <p>Answer</p>
+              <p>Training is a full academic year. We do a three-day induction in August and then the course starts in September and runs until the end of June.</p>
             </ul>
           </li>
 
           <li>
             <a href="#">When should I apply?</a>
             <ul class="menu vertical nested">
-              <p>Answer</p>
+              <p>The application cycle runs from October to August but we would advise applying as early as possible as places are limited and it allows extra time to prepare for the course.</p>
             </ul>
           </li>
 
           <li>
             <a href="#">What are you looking for in a trainee teacher?</a>
             <ul class="menu vertical nested">
-              <p>Answer</p>
+              <p>We are looking for people with enthusiasm and a passion for teaching and make a difference to young people’s lives.</p>
             </ul>
           </li>
 
           <li>
             <a href="#">Who do you train?</a>
             <ul class="menu vertical nested">
-              <p>Answer</p>
+              <p>Our cohorts are a nice blend of learners ranging from 21-55. We train newly-qualified graduates, TAs, LSAs, Cover Supervisors, career changers and career returners. We welcome all applications and believe everyone brings their own unique skill set into the teaching profession.</p>
             </ul>
           </li>
 
           <li>
             <a href="#">Do you offer the Assessment Only route?</a>
             <ul class="menu vertical nested">
-              <p>Answer</p>
+              <p>Yes, we offer the <a href="assessmentonly.php">Assessment Only (AO) route</a> for very experienced classroom practitioners who can demonstrated that they meet the standards for QTS without teacher training.</p>
             </ul>
           </li>
 
